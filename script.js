45dac45e70ac2e0d43d5f438a6146a922c913552
Vivek-Jayaswal <141914455+Vivek-Jayaswal@users.noreply.github.com>
2024-05-03 15:08:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const button = document.getElementById("btn");
+const output = document.getElementById("output");
+
+button.addEventListener("click", () => {
+	const text = document.getElementById("text").value;
+	const delay = document.getElementById("delay").value;
+	displayText(text,delay);
+})
+
+async function displayText(text,delayTime){
+	await new Promise((resolve) => {
+		setTimeout(() => {
+			output.innerText = text;
+		},delayTime);
+	})
+}
